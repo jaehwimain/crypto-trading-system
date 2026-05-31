32d264594cfd9d05871693b0129fe3ed606f08d1
your-github-jaehwimain <jaehwimain@gmail.com>
2026-05-31 18:56:50 +0900

    test: SimulationEngine import 복구 테스트

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ from config import (
 from reporter.telegram_reporter import TelegramReporter
 #from reporter.telegram_command_handler import TelegramCommandHandler
 from optimizers.continuous_optimizer import ContinuousOptimizer
-#from simulator.simulation_engine import SimulationEngine
+from simulator.simulation_engine import SimulationEngine
 
 # Flask 앱 생성
 app = Flask(__name__)
