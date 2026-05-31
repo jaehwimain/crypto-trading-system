ba8d6dac2d4b590ed840199d73357b765a8aba0d
your-github-jaehwimain <jaehwimain@gmail.com>
2026-05-31 19:10:40 +0900

    fix: TelegramCommandHandler import 제거

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ from config import (
 )
 
 from reporter.telegram_reporter import TelegramReporter
-from reporter.telegram_command_handler import TelegramCommandHandler
+#from reporter.telegram_command_handler import TelegramCommandHandler
 from optimizers.continuous_optimizer import ContinuousOptimizer
 #from simulator.simulation_engine import SimulationEngine
 
